d37ba3c450b5e0a0372969c3b685bbf7b0030201
Christian Melendez <camelendez@uc.cl>
2018-06-10 18:07:54 -0400

    added LK function | load_data return X and Y0

--- utils.py
+++ utils.py
@@ -2,11 +2,15 @@ import pandas as pd
 import numpy as np
 import os
 
-def load_data(data_path, n_plants, resample_rule=None):
+def load_data(data_path, n_plants, p, resample_rule=None):
     """
     data_path: directory where the data is saved.
-    n_plants: number of plants to load.
+    n_plants: number of plants to load (K).
     resample: resample rule for data aggregation.
+
+    Return:
+     - data: Y0
+     - X (book X expression)
     """
     data = [None] * n_plants
     for path, _, file_names in os.walk(data_path):
@@ -19,6 +23,22 @@ def load_data(data_path, n_plants, resample_rule=None):
                 data[i] = data[i].resample(resample_rule).mean()
 
             data[i] = data[i]['85m_speed'].values
+            print(data[i].shape)
 
     data = np.stack(data, axis=0)
-    return data
+    
+    # test = data
+    
+    if p > 0:
+        X = np.zeros((n_plants * p, data.shape[1] - p))
+        j = 0
+        for i in range(p, data.shape[1]):
+            for t in range(p):
+                X[t * n_plants:(t + 1) * n_plants, j] = data[:, (i - 1) - t]
+            j += 1
+    else:
+        X = data
+    
+    data = data[:, p:]
+    
+    return data, X
